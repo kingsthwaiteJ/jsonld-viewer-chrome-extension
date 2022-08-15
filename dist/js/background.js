93e90eb225afba5b297d79f10386a29aa93e6ead
James Kingsthwaite <james.k@cadpro.co.nz>
2022-08-15 14:59:41 +1200

    Removed unused icons and updated the title + header of the onboarding page

--- dist/js/background.js
+++ dist/js/background.js
@@ -202,7 +202,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkjson_ld_viewer"] = self["webpackChunkjson_ld_viewer"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
